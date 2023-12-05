4b9d868d47fc68376682450fa53bbc03ce9d9c1c
Fernando Caldera <fer.caldera@gmail.com>
2023-12-05 23:29:33 +0200

    Add new home page

--- lib/stop_web/controllers/page_controller.ex
+++ lib/stop_web/controllers/page_controller.ex
@@ -2,8 +2,8 @@ defmodule StopWeb.PageController do
   use StopWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    conn
+    |> assign(:styles, ~p"/styles/home.css")
+    |> render(:home)
   end
 end
